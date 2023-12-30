0c3c6f47c0fb92c84bdcdf9622fb06de3ea72375
lisaifei <lisaifei@cvte.com>
2023-12-30 09:49:15 +0800

    chore init

---
+++ src/layouts/basic.tsx
@@ -0,0 +1,12 @@
+import { use } from "react";
+import { getInitialData } from "~/services/root";
+
+export async function BasicLayout(props: { children: React.ReactNode }) {
+  const initialData = await getInitialData();
+  return (
+    <>
+      {initialData.data.title}
+      {props.children}
+    </>
+  );
+}
